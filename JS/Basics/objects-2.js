c20aab3dcf34a8865a341348f94b156686df6f95
Igor <sergienko339@gmail.com>
2024-02-26 21:55:07 +0200

    objects l3

--- JS/Basics/objects-2.js
+++ JS/Basics/objects-2.js
@@ -203,3 +203,36 @@ function tournament(obj1, obj2) {
 console.log(
   tournament({ message: "Hello", a: 1, b: "14" }, { a: 10, c: "World" })
 );
+
+const data = {
+  a: 100,
+  b: 300,
+  c: 400,
+  d: 500,
+  e: 600,
+  f: 700,
+  g: 800,
+};
+
+// За допомогою циклу опрацюйте данні так, щоб менші значення були у lowData,
+// більші у bigData
+
+function dataManagement(data, border = 300) {
+  const lowData = {};
+  const bigData = {};
+
+  for (const key in data) {
+    const value = data[key];
+
+    if (value <= border) {
+      lowData[key] = value;
+    } else {
+      bigData[key] = value;
+    }
+  }
+
+  console.log(lowData, bigData);
+  return { lowData, bigData };
+}
+
+console.log(dataManagement(data));
