ffb7e22cca4a43a953067d1aa06f835f50719a01
Igor <sergienko339@gmail.com>
2024-02-28 21:53:51 +0200

    objects 4

--- JS/Basics/objects-3.js
+++ JS/Basics/objects-3.js
@@ -199,3 +199,208 @@ obj3.c = 10000;
 obj3.name = "Igor";
 
 console.log(obj1, obj2, obj3);
+
+const animals = {
+  lion123: {
+    id: "lion123",
+    species: "Lion",
+    age: 6,
+    gender: "Male",
+    habitat: "Savannah",
+    weight_kg: 190,
+    diet: "Carnivore",
+    lifespan_years: 12,
+    conservation_status: "Vulnerable",
+    location: {
+      latitude: -1.2921,
+      longitude: 36.8219,
+      region: "Africa",
+    },
+  },
+  elephant456: {
+    id: "elephant456",
+    species: "African Elephant",
+    age: 15,
+    gender: "Female",
+    habitat: "Grasslands",
+    weight_kg: 5000,
+    diet: "Herbivore",
+    lifespan_years: 60,
+    conservation_status: "Vulnerable",
+    location: {
+      latitude: -4.4419,
+      longitude: 15.2663,
+      region: "Africa",
+    },
+  },
+  tiger789: {
+    id: "tiger789",
+    species: "Tiger",
+    age: 8,
+    gender: "Female",
+    habitat: "Tropical Rainforest",
+    weight_kg: 150,
+    diet: "Carnivore",
+    lifespan_years: 10,
+    conservation_status: "Endangered",
+    location: {
+      latitude: 13.4125,
+      longitude: 103.866,
+      region: "Asia",
+    },
+  },
+  panda101: {
+    id: "panda101",
+    species: "Giant Panda",
+    age: 5,
+    gender: "Male",
+    habitat: "Temperate Broadleaf and Mixed Forests",
+    weight_kg: 120,
+    diet: "Herbivore",
+    lifespan_years: 20,
+    conservation_status: "Vulnerable",
+    location: {
+      latitude: 30.5928,
+      longitude: 103.9526,
+      region: "Asia",
+    },
+  },
+  wolf111: {
+    id: "wolf111",
+    species: "Gray Wolf",
+    age: 4,
+    gender: "Female",
+    habitat: "Taiga",
+    weight_kg: 40,
+    diet: "Carnivore",
+    lifespan_years: 13,
+    conservation_status: "Least Concern",
+    location: {
+      latitude: 61.524,
+      longitude: 105.3188,
+      region: "Europe",
+    },
+  },
+  penguin222: {
+    id: "penguin222",
+    species: "Emperor Penguin",
+    age: 12,
+    gender: "Male",
+    habitat: "Antarctic",
+    weight_kg: 40,
+    diet: "Carnivore",
+    lifespan_years: 20,
+    conservation_status: "Near Threatened",
+    location: {
+      latitude: -82.8628,
+      longitude: 135,
+      region: "Antarctica",
+    },
+  },
+  giraffe333: {
+    id: "giraffe333",
+    species: "Giraffe",
+    age: 10,
+    gender: "Male",
+    habitat: "Savannah",
+    weight_kg: 1200,
+    diet: "Herbivore",
+    lifespan_years: 25,
+    conservation_status: "Vulnerable",
+    location: {
+      latitude: -4.4419,
+      longitude: 34.8522,
+      region: "Africa",
+    },
+  },
+  koala444: {
+    id: "koala444",
+    species: "Koala",
+    age: 7,
+    gender: "Female",
+    habitat: "Eucalypt Woodlands",
+    weight_kg: 8,
+    diet: "Herbivore",
+    lifespan_years: 15,
+    conservation_status: "Vulnerable",
+    location: {
+      latitude: -25.2744,
+      longitude: 133.7751,
+      region: "Australia",
+    },
+  },
+  rhino555: {
+    id: "rhino555",
+    species: "White Rhino",
+    age: 20,
+    gender: "Male",
+    habitat: "Grasslands",
+    weight_kg: 2300,
+    diet: "Herbivore",
+    lifespan_years: 40,
+    conservation_status: "Near Threatened",
+    location: {
+      latitude: -23.4162,
+      longitude: 31.3659,
+      region: "Africa",
+    },
+  },
+  orca666: {
+    id: "orca666",
+    species: "Orca",
+    age: 30,
+    gender: "Female",
+    habitat: "Marine",
+    weight_kg: 5000,
+    diet: "Carnivore",
+    lifespan_years: 50,
+    conservation_status: "Data Deficient",
+    location: {
+      latitude: 48.5126,
+      longitude: -123.383,
+      region: "North America",
+    },
+  },
+};
+
+// 1. Поверніть об`єкт лише з тваринами з Африки
+// 2. Порахуйте середню вагу всіх тварин
+// 3. Видаліть тигра зі списку
+
+function africaOnly(animals) {
+  const africaAnimals = {};
+
+  for (const key in animals) {
+    const animal = animals[key];
+
+    if (animal.location.region === "Africa") {
+      africaAnimals[key] = animal;
+    }
+  }
+
+  console.log(africaAnimals, "africa");
+  return africaAnimals;
+}
+
+africaOnly(animals);
+
+function avrgWeight(animals) {
+  const result = {
+    count: 0,
+    sum: 0,
+  };
+
+  for (const key in animals) {
+    const animal = animals[key];
+
+    result.count = result.count + 1;
+    result.sum = result.sum + animal.weight_kg;
+  }
+
+  console.log(result.sum / result.count, "avgWeight");
+  return result.sum / result.count;
+}
+
+avrgWeight(animals);
+
+delete animals.tiger789;
---
+++ JS/Basics/objects-4.js
@@ -0,0 +1,148 @@
+Object;
+
+const car = {
+  name: "Ford",
+  model: "Mustang",
+  price: 40000,
+  color: "black",
+};
+
+// car.vin = 1000020101;
+
+// not allowed
+
+Object.defineProperty(car, "vin2", {
+  value: 7777777777,
+  enumerable: false,
+  writable: true,
+  configurable: true,
+});
+
+console.log(Object.keys(car));
+console.log(Object.values(car));
+
+console.log(car.vin2, "vin2");
+car.vin2 = 9000000000;
+console.log(car.vin2, "vin2");
+
+delete car.vin2;
+console.log("after delete", car);
+
+Object.defineProperty(car, "private", {
+  value: "private",
+});
+
+console.log(car);
+
+for (const key in car) {
+  console.log(key, car[key]);
+}
+
+// // Завдання:
+
+// const obj1 = { a: 10 };
+// // додайте значення b до obj1  так, щоб його ігнорували цикли
+
+// Опишіть метод keys(obj) - він має повертати всі
+// ключі obj через кому у вигляді строки
+
+function keys(obj) {
+  let objectKeys = "";
+
+  for (const key in obj) {
+    objectKeys = objectKeys.concat(`${key}, `);
+  }
+
+  console.log(objectKeys.slice(0, objectKeys.length - 2), "keys");
+  return objectKeys.slice(0, objectKeys.length - 2);
+}
+
+keys(car);
+// a, b, c, d, price, color
+
+// Завдання
+// Написати свій freeze (існуючі ключі НЕ можна міняти)
+const obj2 = { a: 10 };
+// Object.freeze(obj2);
+
+obj2.a = 15;
+
+console.log(obj2);
+
+function freeze(obj) {
+  for (const key in obj) {
+    Object.defineProperty(obj, key, {
+      value: obj[key],
+      writable: false,
+      enumerable: true,
+      configurable: true,
+    });
+  }
+}
+
+freeze(obj2);
+
+obj2.a = 100000;
+obj2.b = 90;
+
+console.log(Object.entries(obj2));
+console.log(Object.fromEntries([["a", 30]]));
+
+console.log({ a: 10 }.hasOwnProperty("a"));
+const obj3 = { b: 10 };
+obj3.hasOwnProperty();
+
+const user = {
+  id: 0,
+  name: "Admin",
+  password: 123243,
+};
+
+const adminUser = {
+  id: 1,
+  name: "Admin",
+  password: 123243,
+  isAdmin: true,
+};
+
+console.log(user.isAdmin);
+console.log(adminUser.isAdmin);
+
+if (user.hasOwnProperty("id")) {
+  console.log(user.id, "USER ID");
+}
+
+if ("id" in user) {
+  console.log(user.id, "USER ID");
+}
+
+if ("loan" in user) {
+}
+
+typeof user === "object";
+
+function hasOwnProperty(object, keyTolookFor) {
+  let keys = ``;
+
+  for (const key in object) {
+    keys = keys + `${key}|`;
+  }
+
+  // console.log(keys, "keys");
+
+  return keys.includes(keyTolookFor);
+}
+
+console.log(hasOwnProperty({ a: 10 }, "b"));
+
+// Destruction of objects
+
+const dataFromServer = {
+  a: 10,
+  b: 10,
+  c: 10,
+};
+
+const { a: a1, b, c } = dataFromServer;
+
+console.log(a1, b, c);
