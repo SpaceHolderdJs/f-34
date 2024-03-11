9e5a074a200a88253fc12f27627938037f09ef73
Igor <sergienko339@gmail.com>
2024-03-11 21:54:35 +0200

    arrays l3

--- JS/Basics/arrays-2.js
+++ JS/Basics/arrays-2.js
@@ -161,3 +161,22 @@ for (const user of users) {
 }
 
 console.log(sum, "age sum");
+
+function HW() {
+  const users = [
+    { name: "Igor", age: 24 },
+    { name: "Oleg", age: 29 },
+    { name: "Alex", age: 30 },
+    { name: "Joe", age: 14 },
+  ];
+
+  let totalLength = 0;
+
+  for (const user of users) {
+    totalLength = totalLength + user.name.length;
+  }
+
+  return totalLength;
+}
+
+console.log(HW(), "total length of the names");
