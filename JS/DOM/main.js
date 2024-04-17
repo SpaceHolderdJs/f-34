39d7527d8c55b79a0b1abc2122290bd38b30693f
Igor <sergienko339@gmail.com>
2024-04-17 21:53:32 +0300

    dom  events

--- JS/DOM/main.js
+++ JS/DOM/main.js
@@ -116,3 +116,24 @@ const headerToRemove = document.getElementById("header");
 
 const element = document.body.removeChild(headerToRemove);
 console.log(element, "element");
+
+// 2. Завдання: створіть 5 різних елементів використовуючи DOM js
+// 3. Видаліть 2 останніх створенних елемента
+// 4. Першому елементу дайте клас “first”
+// 5. Другому елементу дайте Id “second”
+
+const elementNames = ["p", "span", "div", "article", "h1"];
+const elements = elementNames.map((tag) => document.createElement(tag));
+
+// 1
+elements.forEach((element, index) => {
+  element.textContent = index;
+  document.body.appendChild(element);
+});
+
+// 2
+elements.slice(-2).forEach((element) => document.body.removeChild(element));
+// 3
+elements.at(0).className = "first";
+// 4
+elements.at(1).id = "second";
