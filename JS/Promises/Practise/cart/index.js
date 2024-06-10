5214fe26d6ab205a93ee0ccf0e1afb90625a3685
Igor <sergienko339@gmail.com>
2024-06-10 21:48:04 +0300

    promise practise l2

---
+++ JS/Promises/Practise/cart/index.js
@@ -0,0 +1,66 @@
+class Cart {
+  static elements = {
+    container: document.querySelector(".cart-container"),
+  };
+
+  static renderCart = async () => {
+    try {
+      const cartProducts = CartAPI.present();
+
+      if (!cartProducts.length) {
+        Cart.elements.container.innerHTML =
+          "<h1>There is no any products so far</h1>";
+
+        return;
+      }
+
+      const { data: products } = await API.get("/products");
+
+      Cart.elements.container.innerHTML = "";
+
+      cartProducts.forEach((product) => {
+        const { productId, quantity } = product;
+
+        const productData = products.find(
+          (product) => product.id === +productId
+        );
+
+        const { title, description, price, image, category } = productData;
+
+        Cart.elements.container.innerHTML += `
+          <div class="card mb-3" style="max-width: 640px;">
+            <div class="d-flex g-0 gap-3">
+              <div class="col-md-4">
+                <img
+                  src="${image}"
+                  class="img-fluid rounded-start item-image"
+                  alt="${title}"
+                />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">${title}</h5>
+                  <p class="card-text">${description}</p>
+                  <p class="card-text">
+                    <small class="text-body-secondary">${category}</small>
+                  </p>
+                  <strong>
+                    <p>${quantity * price}$</p>
+                  </strong>
+                  <strong>
+                    <p>Items: ${quantity}</p>
+                  </strong>
+                </div>
+              </div>
+            </div>
+          </div>
+        `;
+      });
+    } catch (err) {
+      console.log(err);
+      //   alert(err.response.data);
+    }
+  };
+}
+
+Cart.renderCart();
--- JS/Promises/Practise/main.js
+++ JS/Promises/Practise/main.js
@@ -9,3 +9,56 @@ const API = axios.create({
 
 // /users - add
 // /auth/login - login
+
+class CartAPI {
+  // [Note]: key - productId, value - quantity
+  // https://fakestoreapi.com/docs#:~:text=Show%20output-,Update%20a%20product,-fetch(%27https%3A//fakestoreapi.com/carts
+
+  static cart = {
+    1: 3,
+    4: 3,
+    6: 1,
+  };
+
+  static present = () => {
+    const productsArray = [];
+
+    for (const productId in CartAPI.cart) {
+      productsArray.push({
+        productId,
+        quantity: CartAPI.cart[productId],
+      });
+    }
+
+    return productsArray;
+  };
+
+  static addToCart = (productId, quantity) => {
+    if (CartAPI.cart[productId]) {
+      CartAPI.cart[productId] += quantity;
+      return;
+    }
+
+    CartAPI[productId] = quantity;
+  };
+
+  static removeFromCart = (productId, quantity) => {
+    if (CartAPI.cart[productId]) {
+      CartAPI.cart[productId] -= quantity;
+
+      if (CartAPI.cart[productId] < 0) CartAPI.cart[productId] = 0;
+    }
+  };
+
+  static removeAllFromCart = (productId) => {
+    if (CartAPI.cart[productId]) {
+      delete CartAPI.cart[productId];
+    }
+  };
+
+  static clear = () => {
+    CartAPI.cart = {};
+  };
+
+  static buy = (cart) => {};
+}
