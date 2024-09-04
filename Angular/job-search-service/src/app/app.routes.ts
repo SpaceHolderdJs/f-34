ad5fecaf5a6bb26196c2e4e0153c71500d47766a
Igor <sergienko339@gmail.com>
2024-09-04 22:25:38 +0300

    angular l2

---
+++ Angular/job-search-service/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { JobsListComponent } from './job/jobs-list/jobs-list.component';
+
+export const routes: Routes = [
+    { path: 'jobs', component: JobsListComponent }
+];
