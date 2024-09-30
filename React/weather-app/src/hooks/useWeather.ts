08dbd5df8523722bb8f20ff00bec559181e17d28
Igor <sergienko339@gmail.com>
2024-09-30 22:00:45 +0300

    weather servuce

---
+++ React/weather-app/src/hooks/useWeather.ts
@@ -0,0 +1,20 @@
+import { useCallback } from "react";
+import { API } from "../api";
+
+export const useWeather = () => {
+    const API_KEY = "6409ee75b6ffc020adb31a565296a4bb";
+
+    const getByCityName = useCallback(async (city: string) => {
+        try {
+            const { data } = await API.get(`weather`, {
+                params: { q: city, appid: API_KEY },
+            });
+            return data;
+        } catch (err) {
+            console.log(err);
+            return null;
+        }
+    }, []);
+
+    return { getByCityName };
+};
